ebe6c6c6ca71de60e4a6fc5fa5798c0ee114de6a
Peixotim <pedropeixotovz@gmail.com>
2025-11-06 17:38:37 -0300

    feat: Adiciona todas as configurações da entidade do Users

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,1 +1,19 @@
-export class UserEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRanks } from './user.ranks';
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 120 })
+  name: string;
+
+  @Column({ unique: true, type: 'varchar' })
+  badge_number: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRanks })
+  rank: UserRanks;
+}
