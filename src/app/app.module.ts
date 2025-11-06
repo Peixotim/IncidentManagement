c589f2db9a52749cbf469d80defd4689645cc188
Peixotim <pedropeixotovz@gmail.com>
2025-11-06 17:23:24 -0300

    feat: Configura os dados do app.module.ts para istanciar o banco de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      database: '',
-      username: '',
-      password: '',
-      port: 1231,
-      autoLoadEntities: true,
-      synchronize: true, //Usar apenas em desenvolvimento
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => {
+        const dbPortString = process.env.DB_PORT || '5432';
+
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(dbPortString, 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          autoLoadEntities: true,
+          synchronize: true, // Deixar true somente para desenvolvimento, em produção desativar
+        };
+      },
     }),
     UserModule,
   ],
