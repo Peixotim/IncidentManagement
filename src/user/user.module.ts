c1c6ee62b64c3769a5c6b44ae7cc6f9d44191c55
Peixotim <pedropeixotovz@gmail.com>
2025-11-06 17:38:14 -0300

    feat: Adiciona a importação do typeOrmModule.forFeature()

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
 
 @Module({
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
 })
 export class UserModule {}
